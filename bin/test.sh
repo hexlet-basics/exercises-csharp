3b728d51f1bfc1f04af5a86249e9d59d17f9f974
Stanislav Dzisiak <stanislav.dzisiak@outlook.com>
2022-01-12 18:19:55 +0200

    fix run test with compiler errors

--- bin/test.sh
+++ bin/test.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 
-scriptcs Test.csx 2>&1
+# scriptcs Test.csx 2>&1
+
+# TODO: при возникновении ошибок компиляции в подключаемых файлах с помощью #load
+# выполнение скрипта завершается с кодом 0, то есть не считается ошибкой.
+# Нужно найти грамотное решение. Временное решение с помощью bash:
+
+set -e
+
+{ OUTPUT=$(scriptcs Test.csx 2>&1 | tee /dev/fd/5); } 5>&1
+
+if [ ! -z "$OUTPUT" ]; then
+  MATCHES_COUNT=$(grep -c 'error CS' <<< $OUTPUT)
+  [ $MATCHES_COUNT == "0" ]
+fi
